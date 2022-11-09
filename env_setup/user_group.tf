d98c1b9a086b66fa4bd6241a9e6fe211c7bc721d
jinshuang <niu.jins@northeastern.edu>
2022-11-08 22:18:21 -0800

    set up ima env

---
+++ env_setup/user_group.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.user_one.name
+  ]
+
+  group = aws_iam_group.group.name
+}
+
+resource "aws_iam_group" "group" {
+  name = "kops-group"
+}
+
+resource "aws_iam_group_policy_attachment" "ec2_full" {
+  group      = aws_iam_group.group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "route53_full" {
+  group      = aws_iam_group.group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "s3_full" {
+  group      = aws_iam_group.group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "iam_full" {
+  group      = aws_iam_group.group.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "vpc_full" {
+  group      = aws_iam_group.group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "EventBridge_full" {
+  group      = aws_iam_group.group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+}
+
+resource "aws_iam_user" "user_one" {
+  name = "kops"
+}
+
+# resource "aws_iam_access_key" "access_key" {
+#   user = aws_iam_user.user_one.name
+# }
+
+# output "access_key_id" {
+#   value = aws_iam_access_key.access_key.id
+# }
+
+# data "template_file" "secret" {
+#   template = aws_iam_access_key.access_key.encrypted_secret
+# }
+
+# output "access_key_secret" {
+#   value = data.template_file.secret.rendered
+# }
